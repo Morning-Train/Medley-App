140f4b969529ac434e4b0430ca108c71bb05c324
Mathias Munk <mh@morningtrain.dk>
2025-09-25 12:54:49 +0000

    Edit Enqueue.php

--- app/Hooks/Enqueue.php
+++ app/Hooks/Enqueue.php
@@ -5,32 +5,15 @@ namespace MedleyApp\Hooks;
 use MorningMedley\Hook\Traits\Hookable;
 use MorningMedley\Hook\Classes\Filter;
 use MorningMedley\Hook\Classes\Action;
-use MorningMedley\Application\WpContext\WpContextContract;
 
 class Enqueue
 {
     use Hookable;
 
-    public function __construct(private WpContextContract $wpContext)
-    {
-    }
-
     #[Action('wp_enqueue_scripts')]
-    public function enqueueStyles(): void
+    public function enqueue(): void
     {
-        \wp_enqueue_style(
-            'main-style',
-            asset('build/css/main.css'),
-            [],
-            $this->wpContext->version
-        );
-
-        $details = require dirname(__FILE__, 3) . "/public/build/js/main.asset.php";
-        \wp_enqueue_script(
-            'main-script',
-            asset('build/js/main.js'),
-            $details['dependencies'],
-            $details['version']
-        );
+        \enqueue_style( 'main-style', 'build/css/main.css');
+        \wp_enqueue_script('main-script', 'build/js/main.js');
     }
 }
